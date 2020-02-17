0e01a053e02b6475807a869c9775187d37707da9
Richard Estes <richard.estes48@gmail.com>
2020-02-16 20:27:29 -0800

    Bot is alive

--- python_post_bot.py
+++ python_post_bot.py
@@ -11,5 +11,51 @@ reddit = praw.Reddit(client_id='oRDWYVEIfzVDAg',
 
 print(reddit.read_only)
 
-subreddits = ['ncaabballstreams', 'sdsu']
+subreddits = ['programmerhumor']
 pos = 0
+errors = 0
+
+title = "HD | Boise State vs San Diego State | CBSSN | MISR:1 mbps | Clicks: 2 | English | Mobile : Yes"
+url = "https://www.viprow.net/sports-basketball-online"
+
+def post():
+    global subreddits
+    global pos
+    global errors
+
+    try:
+        subreddit = reddit.subreddit(subreddits[pos])
+        subreddit.submit(title,url=url)
+        print("Posted to " + subreddits[pos])
+
+        pos = pos + 1
+        post()
+
+        if (pos>=len(subreddits) - 1):
+            post()
+        else:
+            print ("Done")
+    except praw.exceptions.APIException as e:
+        print (e.message)
+        if (e.error_type == "RATELIMIT"):
+            delay = re.search("(\d+) minutes?", e.message)
+
+            if delay:
+                delay_seconds = float(int(delay.group(1)) * 60)
+                time.sleep(delay_seconds)
+                post()
+            else:
+                delay = re.search("(\d+) seconds", e.message)
+                delay_seconds = float(delay.group(1))
+                time.sleep(delay_seconds)
+                post()
+    except:
+        errors = errors + 1
+        if (errors > 5):
+            print("Shit, we crashed.")
+            exit(1)
+
+post()
+
+
+
