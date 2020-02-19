746e36056440772b297027235437273ce85da3f3
Richard Estes <35784400+richardestes@users.noreply.github.com>
2020-02-19 10:59:29 -0800

    Update python_post_bot.py

--- python_post_bot.py
+++ python_post_bot.py
@@ -6,7 +6,7 @@ reddit = praw.Reddit(client_id='oRDWYVEIfzVDAg',
                      client_secret='DkfD4aB3VvrXExaJbSALR_hCmlc',
                      user_agent='<console:ncaa_stream_app:0.0.1 (by /u/sdsu-stream-bot)>',
                      username='sdsu-stream-bot',
-                     password='SDDeveloper42!'
+                     password=''
                      )
 
 print(reddit.read_only)
