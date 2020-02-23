19067a025d80f1fe1191fb234c4e076b9cd6beb7
Richard Estes <richard.estes48@gmail.com>
2020-02-22 16:17:19 -0800

    updated with sportsreference api and post title parsing

--- main.py
+++ main.py
@@ -4,13 +4,23 @@ import time
 import requests
 import getpass
 from security import encrypt_password, check_encrypted_password
+from sports import check_if_game_today
 
-botname = input("Enter bot user name: (sdsu-stream-bot)")
+# Bot Credential Setup 
+# botname = input("Enter bot user name: (sdsu-stream-bot)")
+botname = "sdsu-stream-bot"
 unencrypted_password = getpass.getpass("Enter the bot password: ")
 hash = encrypt_password(unencrypted_password)
-check_encrypted_password(unencrypted_password, hash)
-print(check_encrypted_password(unencrypted_password, hash))
+print("Checking if password was encrypted successfully...")
 
+if(check_encrypted_password(unencrypted_password, hash)):
+    print("Password successfully encrypted.")
+else:
+    print("Uh oh, password was not encrypted correctly. Exiting...")
+    exit(1)
+
+# Bot Creation
+print("Connecting to Reddit...")
 reddit = praw.Reddit(client_id='oRDWYVEIfzVDAg',
                      client_secret='DkfD4aB3VvrXExaJbSALR_hCmlc',
                      user_agent='<console:ncaa_stream_app:0.0.1 (by /u/sdsu-stream-bot)>',
@@ -22,21 +32,50 @@ reddit = praw.Reddit(client_id='oRDWYVEIfzVDAg',
 # We want it to be false
 print("Checking if Reddit praw obj is in read only mode...")
 if (reddit.read_only == True):
-    print("Uh oh, looks like Reddit is in read only mode. Exiting.")
+    print("Uh oh, looks like Reddit is in read only mode. Exiting...")
     exit(1)
 else:
-    print("Everything looks good!")
+    print("Reddit is not in read only mode. Continuing...")
 
-# # subreddit = reddit.subreddit('ncaaBBallStreams')
-# subreddit = reddit.subreddit('programmerhumor')
+check_if_game_today()
+
+# subreddit = reddit.subreddit('SecretSharedDawn')
 
 # for submission in subreddit.stream.submissions():
-#     # do something with submission
-
-#     if "San Diego State" in submission.title:
-#         # Get comments / create comment in submission
-#         print("Submission found. Replying with link...")
-#         reply_text = "**HD** | [Boise State vs San Diego State | CBSSN](https://www.viprow.net/sports-basketball-online) | MISR:1 mbps | Clicks: 2 | English | Mobile : Yes"
+#     if "test post" in submission.title:
+#         print("Submission found. Replying...")
+#         reply_text = "hello i am a bot!"
+#         submission.reply(reply_text)
+#         print("Replied to post.")
 #         break
 #     else:
-#         continue
+#          continue
+
+subreddit = reddit.subreddit('ncaaBBallStreams')
+
+for submission in subreddit.stream.submissions():
+    # do something with submission
+
+    if "San Diego St" in submission.title:
+        split_title = submission.title.split("vs")
+        reddit_title_left = split_title[0]
+        # print(reddit_title_left)
+        split_colon = reddit_title_left.split(":")
+        # print(split_colon[1])
+        reddit_team_name_left = split_colon[1].lstrip()
+        print(reddit_team_name_left)
+        r_vs = re.compile(r"^\W+")  
+        tmp_right = r_vs.sub("",split_title[1])
+        reddit_title_right = tmp_right.split()
+        reddit_team_name_right = reddit_title_right[0]
+        print(reddit_team_name_right)
+        # Get comments / create comment in submission
+        print("Submission found. Replying with link...")
+        game_title = reddit_team_name_left + "vs " + reddit_team_name_right
+        print(game_title)
+        reply_text = "**HD** | [" + game_title + "](https://www.viprow.net/sports-basketball-online) | Clicks: 2 | English | Mobile : Yes"
+        print(reply_text)
+        submission.reply(reply_text)
+        break
+    else:
+        continue
---
+++ sports.py
@@ -0,0 +1,34 @@
+from sportsreference.ncaab.schedule import Schedule
+from sportsreference.ncaab.teams import Teams
+import datetime
+
+# teams = Teams()
+# for team in teams:
+#     print(team.name, team.abbreviation)
+
+def check_if_game_today():
+    now = datetime.datetime.now()
+    # date_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    now_date_time = now.strftime("%Y-%m-%d")
+
+    print("Current date:")
+    print(now_date_time)
+    print()
+
+    sdsu_schedule = Schedule('san-diego-state')
+    for game in sdsu_schedule:
+        game_date = game.datetime.strftime("%Y-%m-%d")
+        game_hour = game.datetime.strftime("%H")
+        game_hour_int = int(game_hour)
+        game_hour_pst = game_hour_int - 3
+        if (now_date_time == game_date):
+            print("Hey there's a game today!")
+            print(game.datetime)
+            print(game_date)
+            print(game_hour_pst)
+            print(game.opponent_name)
+
+
+
+
+    
