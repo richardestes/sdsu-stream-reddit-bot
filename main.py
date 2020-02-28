d56e697384f98d97f75705a8f8d9792c860e2a70
Richard Estes <richard.estes48@gmail.com>
2020-02-28 14:24:05 -0800

    added emojis because god is dead

--- main.py
+++ main.py
@@ -66,15 +66,15 @@ def test_submission(reddit_username,reddit_password):
 def post_sdsu_stream(reddit_username,reddit_password):
 
     hash = encrypt_password(reddit_password)
-    print("Checking if password was encrypted successfully...")
+    print("Checking if password was encrypted successfully...\U0001F928")
     if(check_encrypted_password(reddit_password, hash)):
         print("Password successfully encrypted.")
     else:
-        print("Uh oh, password was not encrypted correctly. Exiting...")
+        print("Uh oh, password was not encrypted correctly. Exiting...\n")
         exit(1)
 
     # Bot Creation
-    print("Connecting to Reddit...")
+    print("Seeing if we can connect to reddit...\U0001F914")
     reddit = praw.Reddit(client_id='oRDWYVEIfzVDAg',
                         client_secret='DkfD4aB3VvrXExaJbSALR_hCmlc',
                         user_agent='<console:ncaa_stream_app:0.0.1 (by /u/sdsu-stream-bot)>',
@@ -83,12 +83,13 @@ def post_sdsu_stream(reddit_username,reddit_password):
                         )
     # Check to see if post is in read only mode
     # We want it to be false
+    print("Wow we connected to reddit awesome")
     print("Checking if Reddit praw obj is in read only mode...")
     if (reddit.read_only == True):
-        print("Uh oh, looks like Reddit is in read only mode. Exiting...")
+        print("Uh oh, looks like Reddit is in read only mode. Exiting...\n")
         exit(1)
     else:
-        print("Reddit is not in read only mode. Continuing...")       
+        print("Reddit is not in read only mode. Continuing...\n")       
 
     if check_if_game_today():
 
@@ -110,21 +111,21 @@ def post_sdsu_stream(reddit_username,reddit_password):
                 reply_text = "**HD** | ["+game_title_no_time+"](https://www.viprow.me/sports-basketball-online) | Clicks: 2 | English | Disable Adblock"
                 print(reply_text)
                 # submission.reply(reply_text)
-                print("Replied to submission. Going to sleep for 24 hours zzz...")
+                print("Replied to submission. \U0001F60D My job here is done. Going to sleep for 24 hours zzz...\U0001F634 \n")
                 create_progress_bar(86400)
                 break
             else:
                 continue
 
     else:
-        print("Game not found! Going to sleep for 2 hours zzz...")
+        print("Game not found! Going to sleep for 2 hours zzz... \U0001F634 \n")
         create_progress_bar(7200)
 
 
 while True:
     try:
         post_sdsu_stream(botname,unencrypted_password)
-    except Exception as e:
+    except Exception as e: #probably an APIException error
         logging.basicConfig(filename='error_log.txt',level=logging.ERROR)
         logging.error(e)
         create_progress_bar(900)
